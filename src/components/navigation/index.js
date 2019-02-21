58542bcad2fb61e3037ba3130f68295f698df3d6
Bradley <pickbradley23@gmail.com>
2019-02-20 18:58:11 -0800

    Implement dropdowns in nav bar

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './agency.css';
 
 import * as ROUTES from '../../constants/routes';
 
+class Navigation extends React.Component{
+  render() {
+    return (
+      
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div className="container">
+          <a className="navbar-brand js-scroll-trigger" href="#page-top"><img src='./soles-sun.png' alt = "Soles Sun" style={{height: '100px'}} /></a>
+
+            <div className="dropdown">
+              <button className="dropbtn"><Link to={ROUTES.SIGN_UP}>Students</Link></button>
+              <div className="dropdown-content">
+                <a href="#">How to Join</a>
+                <a href="#">Calendar</a>
+                <a href="#">Contact</a>
+              </div>
+            </div>
+
+            <div className="dropdown">
+              <button className="dropbtn"><Link to={ROUTES.HOME}>Companies</Link></button>
+            </div>
+
+            <div className="dropdown">
+              <button className="dropbtn"><Link to={ROUTES.LANDING}>About</Link></button>
+              <div className="dropdown-content">
+                <a href="#">What We Do</a>
+                <a href="#">Sponsors</a>
+                <a href="#">History</a>
+              </div>
+            </div>
+
+            <div className="dropdown">
+              <button className="dropbtn"><Link to={ROUTES.SIGN_IN}>Login</Link></button>
+            </div>
+            
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+          </div>
+        </div>
+      </nav>
+
+      
+
+    );
+  }
+}
+
+/*
 const Navigation = () => (
   <div>
     <ul>
@@ -24,5 +71,5 @@ const Navigation = () => (
     </ul>
   </div>
 );
-
+*/
 export default Navigation;
