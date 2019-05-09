bd6a2d46cd4d87258168c47f538d4e04359d9ac7
Bradley <pickbradley23@gmail.com>
2019-05-08 17:54:51 -0700

    Tests passed in by props are displayed as list

--- src/components/testbank/index.js
+++ src/components/testbank/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import Test from "../../components/test"
 
 import { AuthUserContext, withAuthorization } from '../session';
 import * as ROUTES from '../../constants/routes';
 
-const TestBankPage = () => (
-  <AuthUserContext.Consumer>
-    {authUser => (
-      <div>
-        <h1>Test Bank</h1>
-      </div>
-    )}
-  </AuthUserContext.Consumer>
-);
+function TestBankPage(props) {
+  return (
+    <AuthUserContext.Consumer>
+      {authUser => (
+        <div>
+          {props.tests.map(t => 
+            <Test key={t.id} professor={t.professor} year={t.year} quarter={t.quarter} type={t.type} />
+          )}
+      </div> 
+      )}
+    </AuthUserContext.Consumer>
+  );
+}
 
 const condition = authUser => !!authUser;
 
-export default withAuthorization(condition)(TestBankPage);
+//export default withAuthorization(condition)(TestBankPage);
+export default TestBankPage;
