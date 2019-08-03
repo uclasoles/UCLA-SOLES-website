eed89a623884c644be47cab2097e6c42ee235be4
Kyle Romero <kyleromero98@gmail.com>
2019-08-03 15:11:32 -0700

    Updated subscribe footer frontend

--- src/components/subscribefooter/subscribefooter.js
+++ src/components/subscribefooter/subscribefooter.js
@@ -1,37 +1,50 @@
-import React, { Component } from 'react';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import '../../custom.css';
-import '../../fontawesome.js';
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import './subscribefooter.css'
+import '../../fontawesome.js'
 
 class SubscribeFooter extends Component {
+  state = {
+    email: '',
+    error: null
+  }
+
+  constructor (props) {
+    super(props)
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.subscribeEmail(this.state);
+	};
+
+  onChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  };
+
   render () {
+    const { email } = this.state
+
     return (
       <div id="subscribe-footer">
-        <Container id="subscribe-form">
-          <Row>
-            <h2>Join our weekly newsletter!</h2>
-          </Row>
-          <Row>
-            <Form>
-              <Form.Group>
-                <Form.Row>
-                  <Col>
-                    <InputGroup>
-                      <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">Subscribe</InputGroup.Text>
-                      </InputGroup.Prepend>
-                      <Form.Control as="input" placeholder="Email"></Form.Control>
-                    </InputGroup>
-                  </Col>
-                </Form.Row>
-              </Form.Group>
-            </Form>
-          </Row>
-        </Container>
+        <div className="row h-100 justify-content-center align-items-center">
+					<Card style={{ width:'30rem', color: 'black'  }}>
+            <Card.Header as="h3">Weekly Newsletter</Card.Header>
+            <Card.Body>
+              <Form onSubmit={this.onSubmit}>
+                <p><i>Keep up to date with the latest SOLES news.</i></p>
+                
+                <Form.Group controlId="formSubscribeEmail">
+                  <Form.Control name="email" value={email} onChange={this.onChange} type="email" placeholder="Email Address"/>
+                </Form.Group>
+
+                <Button type="submit" variant='primary' block>Sign In</Button>
+              </Form>
+            </Card.Body>
+					</Card>
+				</div>
       </div>
     )
   } 
